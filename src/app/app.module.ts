d8a94db822ea476d758c3a3ed233fa35fdf3d360
Calum MacUisdean <calum@martinmacuisdean.eu>
2017-06-20 11:52:20 +0100

    Fixed Google Analytics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from "@angular/router";
 import { BrowserModule, Title } from '@angular/platform-browser';
+import { Angulartics2Module, Angulartics2GoogleAnalytics } from 'angulartics2';
 
 /* Components */
 import { AppComponent } from './app.component';
@@ -21,6 +22,7 @@ import { firebaseConfig } from './configuration/firebase';
         BrowserModule,
         HttpModule,
         RouterModule.forRoot(rootRouterConfig),
+        Angulartics2Module.forRoot([Angulartics2GoogleAnalytics])
     ],
     declarations: [
         AppComponent,
